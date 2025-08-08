e086fe6d1048e443bc93ef20307ed06051290b7a
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2025-08-08 15:08:57 +0530

    Add liveAtStation and searchTrainBetweenStations functions to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ declare module 'irctc-connect' {
     export function checkPNRStatus(pnr: string): Promise<any>;
     export function getTrainInfo(trainNumber: string): Promise<any>;
     export function trackTrain(trainNumber: string, date: string): Promise<any>;
+    export function liveAtStation(stationCode: string): Promise<any>;
+    export function searchTrainBetweenStations(fromStnCode: string, toStnCode: string): Promise<any>;
   }
