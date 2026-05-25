5646a6066442e064b069685b6a13569d4171e053
Rajiv Dubey <lucky81205@gmail.com>
2026-05-25 22:03:00 +0530

    feat: integrate Google Tag Manager for enhanced tracking

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -96,6 +96,15 @@ export default function RootLayout({
       <head>
         <script
           dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WCXRHWZD');`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
             __html: `
               (function() {
                 try {
@@ -112,6 +121,14 @@ export default function RootLayout({
         />
       </head>
       <body className={`${notoSans.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WCXRHWZD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider>
           <Header />
           {children}
